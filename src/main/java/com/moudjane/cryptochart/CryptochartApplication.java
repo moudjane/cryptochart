e82b69dea91b9bd1844f32c3c44a755d67465e8a
Moudjane <mathis.oudjane@gmail.com>
2024-02-25 01:48:41 +0100

    project creation

---
+++ src/main/java/com/moudjane/cryptochart/CryptochartApplication.java
@@ -0,0 +1,13 @@
+package com.moudjane.cryptochart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptochartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptochartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moudjane/cryptochart/CryptochartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moudjane.cryptochart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptochartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
